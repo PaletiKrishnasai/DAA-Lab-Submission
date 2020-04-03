e151e2bf5501bef70739d4f457eae77c6362eb71
Yoga Sri Varshan V <yogasrivarshan@gmail.com>
2020-04-03 12:06:25 +0530

    Update MST_Kruskal.h

--- L8/module5/MST_Kruskal.h
+++ L8/module5/MST_Kruskal.h
@@ -1,81 +1,85 @@
-// Simple C++ implementation for Kruskal's 
-// algorithm 
-#include <bits/stdc++.h> 
-using namespace std; 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+bool comparePairs(const std::pair<pair<int,int>,int>& lhs, const std::pair<pair<int,int>,int>& rhs){
+  return lhs.second < rhs.second;
+}
+ll visit[100] = {0};
+ll checkCycle(vector<vector<ll>>t,ll v,ll u){
+ 	ll i;
+	visit[v] = 1;
+	for(i=0;i<t.size();i++){
+		if(t[v][i]!=0){
+			if(visit[i]==1 && i!=u)
+				return 1;
+			else{
+				if(visit[i]==0){
+					return checkCycle(t,i,v);
+				}
+				else
+					return 0;
+			}
+		}
+	}
+	return 0;
+}
+vector<pair<pair<ll,ll>,ll>>edges;
+vector<pair<pair<ll,ll>,ll>>MST;
+void MST_Kruskal(vector<vector<ll>>g){
+	ll i,j,n = g.size();
+	vector<vector<ll>>t(n);
+	for(i=0;i<n;i++){
+		vector<ll>temp(n);
+		for(j=0;j<n;j++)
+			temp[i]=0;
+		t[i]=temp;
+	}
+	for(i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+			pair<ll,ll>p;
+			p.first = i;
+			p.second = j;
+			if(g[i][j]!=0){
+				pair<pair<ll,ll>,ll>p2;
+				p2.first = p;
+				p2.second = g[i][j];
+				edges.push_back(p2);
+			}
+		}
+	}
+	sort(edges.begin(),edges.end(),comparePairs);
+	for(auto it = edges.begin();it!=edges.end();it++){
+		cout<<it->first.first<<","<<it->first.second<<" "<<it->second<<endl;
+		t[it->first.first][it->first.second]=it->second;
+		t[it->first.second][it->first.first]=it->second;
+		for(i=0;i<n;i++){
+			vector<ll>temp(n);
+			temp=t[i];
+			for(j=0;j<n;j++)
+				cout<<temp[j]<<" ";
+			cout<<endl;
+		}
+		visit[100]={0};
+		if(checkCycle(t,it->first.first,it->first.first)==0){
+			cout<<"include"<<endl;
+			MST.push_back(*it);
+		}
+		else{
+			t[it->first.first][it->first.second]=0;
+			t[it->first.second][it->first.first]=0;
+		}
+	}
+	cout<<"The MST has edges -"<<endl;
+	for(auto it = MST.begin();it!=MST.end();it++){
+		cout<<it->first.first<<","<<it->first.second<<" "<<it->second<<endl;
 
-#define V 5 
-int parent[V]; 
-
-// Find set of vertex i 
-int find(int i) 
-{ 
-	while (parent[i] != i) 
-		i = parent[i]; 
-	return i; 
-} 
-
-// Does union of i and j. It returns 
-// false if i and j are already in same 
-// set. 
-void union1(int i, int j) 
-{ 
-	int a = find(i); 
-	int b = find(j); 
-	parent[a] = b; 
-} 
-
-// Finds MST using Kruskal's algorithm 
-void kruskalMST(int cost[][V]) 
-{ 
-	int mincost = 0; // Cost of min MST. 
-
-	// Initialize sets of disjoint sets. 
-	for (int i = 0; i < V; i++) 
-		parent[i] = i; 
-
-	// Include minimum weight edges one by one 
-	int edge_count = 0; 
-	while (edge_count < V - 1) { 
-		int min = INT_MAX, a = -1, b = -1; 
-		for (int i = 0; i < V; i++) { 
-			for (int j = 0; j < V; j++) { 
-				if (find(i) != find(j) && cost[i][j] < min) { 
-					min = cost[i][j]; 
-					a = i; 
-					b = j; 
-				} 
-			} 
-		} 
-
-		union1(a, b); 
-		printf("Edge %d:(%d, %d) cost:%d \n", 
-			edge_count++, a, b, min); 
-		mincost += min; 
-	} 
-	printf("\n Minimum cost= %d \n", mincost); 
-} 
-
-// driver program to test above function 
-int main() 
-{ 
-	/* Let us create the following graph 
-		2 3 
-	(0)--(1)--(2) 
-	| / \ | 
-	6| 8/ \5 |7 
-	| /	 \ | 
-	(3)-------(4) 
-			9		 */
-	int cost[][V] = { 
-		{ INT_MAX, 2, INT_MAX, 6, INT_MAX }, 
-		{ 2, INT_MAX, 3, 8, 5 }, 
-		{ INT_MAX, 3, INT_MAX, INT_MAX, 7 }, 
-		{ 6, 8, INT_MAX, INT_MAX, 9 }, 
-		{ INT_MAX, 5, 7, 9, INT_MAX }, 
-	}; 
-
-	// Print the solution 
-	kruskalMST(cost); 
-
-	return 0; 
-} 
+	}
+	cout<<"The MST graph is -"<<endl;
+	for(i=0;i<n;i++){
+		vector<ll>temp(n);
+		temp=t[i];
+		for(j=0;j<n;j++)
+			cout<<temp[j]<<" ";
+		cout<<endl;
+	}
+}
